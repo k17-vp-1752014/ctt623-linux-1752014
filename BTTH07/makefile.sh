7e8895b0aef56a865f138d450732db24c851abe7
k17-vp-1752014 <40013630+k17-vp-1752014@users.noreply.github.com>
2018-08-17 02:42:01 +0700

    Add files via upload

---
+++ BTTH07/makefile.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+makeAMakeFile ()
+{
+    echo -n -e "all: exo \nexo: "
+    for i in $*; do 
+	echo -n "${i/.c/.o}"
+	done
+    echo -n -e "\n\tgcc "
+    for i in $*; do 
+	echo -n "${i/.c/.o}"
+	done
+    echo " -o exo"
+    for i in $*; do
+	echo -e "${i/.c/.o}: $i\n\t gcc -c $i"
+	done
+    echo -e "clean:\\n\trm -f*.o exo\ndepend:\n\tmakedepend *.c"
+}
+
+makeAMakeFile BTTH07a.c BTTH07b.c
